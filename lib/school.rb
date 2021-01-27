0faa1ad26fa357162652115a66677b955426cc41
pbenton6 <patrick.benton6396@gmail.com>
2021-01-27 16:14:48 -0600

    commit tree 303e6cc323674f2cd63f1bd5753a21baa4baa43b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+    
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name 
+    end
+    
+    def grade(grade_level)
+        roster[grade_level]
+    end
+
+    def sort
+        alphabetical = {}
+        roster.each do |grade, name|
+            alphabetical[grade] = name.sort
+        end
+        alphabetical
+    end
+
+end
